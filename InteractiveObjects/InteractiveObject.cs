fbfe25cf9aa6ceaf75c49dd9d72db8830bced182
July Sudarenko <july@sudarenko.ru>
2020-11-25 09:39:04 +0300

    Gizmos

--- InteractiveObjects/InteractiveObject.cs
+++ InteractiveObjects/InteractiveObject.cs
@@ -4,17 +4,19 @@ using Random = UnityEngine.Random;
 
 namespace Labyrinth
 {
-    public abstract class InteractiveObject : MonoBehaviour//, IExecute
+    public abstract class InteractiveObject : MonoBehaviour
     {
         #region Field
 
         public bool IsFly;
-        
+
         protected PlayerBase _player;
         protected Color _color;
         protected Renderer _renderer;
         protected Collider _collider;
 
+        [SerializeField] private bool _isAllowScaling;
+        [SerializeField] private float ActiveDis;
         private bool _isInteractable;
 
         public bool IsInteractable
@@ -58,13 +60,31 @@ namespace Labyrinth
             IsInteractable = false;
         }
 
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawIcon(transform.position, "Bonus.png", _isAllowScaling);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            #if UNITY_EDITOR
+            Transform t = transform;
+
+            //Gizmos.matrix = Matrix4x4.TRS(t.position, t.rotation, t.localScale);
+            //Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+
+            var flat = new Vector3(ActiveDis, 0, ActiveDis);
+            Gizmos.matrix = Matrix4x4.TRS(t.position, t.rotation, flat);
+            Gizmos.DrawWireSphere(Vector3.zero, 5);
+            #endif
+        }
+
         #endregion
 
 
         #region Methods
 
         protected abstract void Interaction();
-        //public abstract void Execute();
 
         #endregion
     }
